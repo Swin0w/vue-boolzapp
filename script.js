15d4af490660ddc5eef45ec9c35c94dc887ab5c2
Simone <marzolla.istruzione@gmail.com>
2021-04-09 18:12:08 +0200

    Caricamento contatti

--- script.js
+++ script.js
@@ -2,18 +2,47 @@ function init(){
   new Vue({
     el: "#navContacts",
     data: {
-      contacs: [
-      {
-        nome: "",
-        avatar: "",
-        messages: [
-          {
-            date: new Date(2021, 12, 7),
-            text:"",
-            status: ""
-          }
-        ]
-      }
+      contacts: [
+        {
+          name: "Mamma",
+          avatar: "_1",
+          messages: [
+            {
+              date: "05/04/2021",
+              text: "Oi ma, mi vai a comprare degli Estathè?",
+              status: "sent"
+
+            },
+            {
+              date: "05/04/2021",
+              text: "Ok va bene",
+              status: "received"
+            },
+            {
+              date: "05/04/2021",
+              text: "Grazie mille",
+              status: "sent"
+            }
+          ]
+        },
+        {
+          name: "Gabriele",
+          avatar: "_2",
+          messages: [
+            {
+              date: "07/04/2021",
+              text: "Oi ciao Luca, ti va se ci incontriamo?",
+              status: "received"
+            },
+            {
+              date: "07/04/2021",
+              text: "Si va benessimo, vieni da me per le 16.30",
+              status: "sent"
+            }
+          ]
+        }
+      ]
     }
-  });
+
+    });
 }
